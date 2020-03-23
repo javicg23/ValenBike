97925bc6a84f0fd371530f901e0fdf7ee5499fc3
Victor Cardona <vicardona7@hotmail.com>
2020-03-23 16:40:37 +0100

    added map and merge with information

--- app/src/main/java/disca/dadm/valenbike/fragments/MapFragment.java
+++ app/src/main/java/disca/dadm/valenbike/fragments/MapFragment.java
@@ -4,26 +4,44 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import disca.dadm.valenbike.R;
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback{
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    private GoogleMap map;
 
-    /**
-     * Creates the View associated to this Fragment from a Layout resource.
-     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_map, container, false);
-        return view;
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+
+        /*
+         * Configure Mapview and sync to google map.
+         */
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        //map.onCreate(savedInstanceState);
+        mapFragment.getMapAsync(this);
+        //mapView.onResume(); // needed to get the map to display immediately
+
+        //your code
+        return rootView;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.map = googleMap;
+
+
     }
 }
