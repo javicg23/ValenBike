464e6874a75dd9873999d8617d0d4ea9081df381
Victor Cardona <vicardona7@hotmail.com>
2020-03-23 21:36:35 +0100

    added search bar in map fragment

--- app/src/main/java/disca/dadm/valenbike/fragments/MapFragment.java
+++ app/src/main/java/disca/dadm/valenbike/fragments/MapFragment.java
@@ -1,47 +1,88 @@
 package disca.dadm.valenbike.fragments;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.IOException;
+import java.util.List;
 
 import disca.dadm.valenbike.R;
+import disca.dadm.valenbike.activities.MainActivity;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback{
 
     private GoogleMap map;
+    private SearchView searchView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_map, container, false);
 
         /*
          * Configure Mapview and sync to google map.
          */
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        //map.onCreate(savedInstanceState);
         mapFragment.getMapAsync(this);
-        //mapView.onResume(); // needed to get the map to display immediately
 
-        //your code
+        searchView = rootView.findViewById(R.id.searchBar);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                String location = searchView.getQuery().toString();
+                List<Address> addressList = null;
+
+                if (location != null || !location.equals("")){
+                    Geocoder geocoder = new Geocoder(getActivity());
+                    try {
+                        addressList = geocoder.getFromLocationName(location,1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    if (addressList != null && addressList.size() != 0) {
+                        Address address = addressList.get(0);
+                        LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
+                        map.addMarker(new MarkerOptions().position(latLng).title(location));
+                        map.animateCamera(CameraUpdateFactory.newLatLngZoom( latLng,13));
+                    } else {
+                        Snackbar snackbar = Snackbar.make(rootView, "Localización no encontrada", Snackbar.LENGTH_SHORT);
+                        snackbar.setAnchorView(R.id.bottomView);
+                        snackbar.show();
+                    }
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
         return rootView;
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         this.map = googleMap;
-
-
     }
 }
