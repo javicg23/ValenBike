41f7a3069e58460934b40a88391af1db5d4acc02
Nerea Fernández <nerea.fernandez@servest.app>
2020-03-29 20:18:34 +0200

    Merge branch 'master' of D:\Documentos\Universidad\Segundo Cuatrimestre\DADM\proyecto with conflicts.

--- app/src/main/java/disca/dadm/valenbike/database/JourneyDao.java
+++ app/src/main/java/disca/dadm/valenbike/database/JourneyDao.java
@@ -1,26 +1,26 @@
-package disca.dadm.valenbike.database;
-
-import androidx.room.Dao;
-import androidx.room.Delete;
-import androidx.room.Insert;
-import androidx.room.OnConflictStrategy;
-import androidx.room.Query;
-import androidx.room.Update;
-
-import java.util.List;
-
-@Dao
-public interface JourneyDao {
-
-        @Query("SELECT * FROM journey")
-        List<Journey> getJourneys();
-
-        @Insert(onConflict = OnConflictStrategy.REPLACE)
-        long addJourney(Journey journey);
-
-        @Update
-        void updateJourney(Journey journey);
-
-        @Delete
-        void deleteJourney(Journey journey);
-}
+//package disca.dadm.valenbike.database;
+//
+//import androidx.room.Dao;
+//import androidx.room.Delete;
+//import androidx.room.Insert;
+//import androidx.room.OnConflictStrategy;
+//import androidx.room.Query;
+//import androidx.room.Update;
+//
+//import java.util.List;
+//
+//@Dao
+//public interface ViajeDao {
+//
+//        @Query("SELECT * FROM viaje")
+//        List<Estacion> getViajes();
+//
+//        @Insert(onConflict = OnConflictStrategy.REPLACE)
+//        long addViaje(Viaje viaje);
+//
+//        @Update
+//        void updateViaje(Viaje viaje);
+//
+//        @Delete
+//        void deleteViaje(Viaje viaje);
+//}
--- app/src/main/java/disca/dadm/valenbike/database/ValenbikeDatabase.java
+++ app/src/main/java/disca/dadm/valenbike/database/ValenbikeDatabase.java
@@ -1,30 +1,27 @@
-package disca.dadm.valenbike.database;
-
-import android.content.Context;
-
-import androidx.room.Database;
-import androidx.room.Room;
-import androidx.room.RoomDatabase;
-import androidx.room.TypeConverter;
-import androidx.room.TypeConverters;
-
-@Database(entities = {Journey.class, Station.class}, version = 1, exportSchema = false)
-@TypeConverters({Converters.class})
-public abstract class ValenbikeDatabase extends RoomDatabase {
-
-    //Patron Singleton
-    private static ValenbikeDatabase valenbikeDatabase;
-
-    public synchronized static ValenbikeDatabase getInstance(Context context) {
-        if (valenbikeDatabase == null) {
-            valenbikeDatabase = Room
-                    .databaseBuilder(context, ValenbikeDatabase.class, "valenbikes")
-                    .build();
-        }
-
-        return valenbikeDatabase;
-    }
-
-    public abstract JourneyDao journeyDao();
-    public abstract StationDao stationDao();
-}
+//package disca.dadm.valenbike.database;
+//
+//import android.content.Context;
+//
+//import androidx.room.Database;
+//import androidx.room.Room;
+//import androidx.room.RoomDatabase;
+//
+//@Database(entities = {Viaje.class, Estacion.class}, version = 1)
+//public abstract class ValenbikeDatabase extends RoomDatabase {
+//
+//    //Patron Singleton
+//    private static ValenbikeDatabase valenbikeDatabase;
+//
+//    public synchronized static ValenbikeDatabase getInstance(Context context) {
+//        if (valenbikeDatabase == null) {
+//            valenbikeDatabase = Room
+//                    .databaseBuilder(context, ValenbikeDatabase.class, "valenbikes")
+//                    .build();
+//        }
+//
+//        return valenbikeDatabase;
+//    }
+//
+//    public abstract ViajeDao viajeDao();
+//    public abstract EstacionDao estacionDao();
+//}
